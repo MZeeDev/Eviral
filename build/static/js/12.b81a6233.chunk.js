e0713625fc8cfb9beb91050af628e791bb826cee
Sol Alch <solidityalchemist@gmail.com>
2022-02-18 13:56:46 -0800

    npm install web3auth and magicsdk

---
+++ build/static/js/12.b81a6233.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpviral-crypto"]=this["webpackJsonpviral-crypto"]||[]).push([[12],{1416:function(n,c){},1497:function(n,c){},1498:function(n,c){}}]);
+//# sourceMappingURL=12.b81a6233.chunk.js.map
---
+++ build/static/js/13.661f6b5c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpviral-crypto"]=this["webpackJsonpviral-crypto"]||[]).push([[13],{1416:function(n,c){},1424:function(n,c){},1450:function(n,c){}}]);
+//# sourceMappingURL=13.661f6b5c.chunk.js.map
